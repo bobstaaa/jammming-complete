87f64f2003c413e9d6d95c1f0826d1307c9234ae
Craig H <92305155+bobstaaa@users.noreply.github.com>
2022-08-01 23:06:33 +1000

    Update search results and search bar components.

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBar from '../components/searchBar/searchBar.js';
 
 function App() {
   return (
---
+++ src/components/searchResults/searchResult.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class SearchResults extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                //Add a TrackList component
+            </div>
+        )
+    }
+}
